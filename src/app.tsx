0831312f7771a666e4df6580b649faa4b4258a65
rambhau waykar <waykar_rambhau.pandurang@syngenta.com>
2022-10-16 14:02:49 +0530

    fix: typescript added successfully

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { FC } from "react";
 
 import ReactLogo from './images/react-logo.svg';
 import NatureImg from './images/nature.png';
 
-export const Welcome = () => {
+export const Welcome: FC = () => {
   return (
     <div className="app">
       <ReactLogo className="react-logo" />
